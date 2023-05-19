3d60d4906b2b8ad0af27c15bbe49f2a091337324
Bartosz Troń <123635290+Bartiiio@users.noreply.github.com>
2023-05-19 15:20:40 +0200

    dodane navbar i panel logowania

--- ekorki/frontend/src/App.vue
+++ ekorki/frontend/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-   <img alt="Vue logo" src="./assets/logo.png" />
-   <HelloWorld msg="Welcome to Your Vue.js App" />
+   <div class="navBar">
+      <navBar />
+   </div>
+   <div class="main">
+      <loginPanel />
+   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import navBar from "./components/navBar.vue";
+import loginPanel from "./components/loginPanel.vue";
 
 export default {
-   name: "App",
    components: {
-      HelloWorld,
+      navBar,
+      loginPanel,
    },
 };
 </script>
-
-<style>
-#app {
-   font-family: Avenir, Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-   text-align: center;
-   color: #2c3e50;
-   margin-top: 60px;
-}
-</style>
