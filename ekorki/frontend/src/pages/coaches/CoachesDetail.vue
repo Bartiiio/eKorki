1f0cb3f927015dff7d9ebdd6623931e72f826453
Bartosz Troń <123635290+Bartiiio@users.noreply.github.com>
2023-05-23 22:44:23 +0200

    drobne zmiany

--- ekorki/frontend/src/pages/coaches/CoachesDetail.vue
+++ ekorki/frontend/src/pages/coaches/CoachesDetail.vue
@@ -1,5 +1,7 @@
 <template>
-Informacje o korepetytorze
-<router-view></router-view>
-<router-link to="/coaches/c1/contact">Contact</router-link>
-  </template>
+   <div>
+      Informacje o korepetytorze
+      <router-view></router-view>
+      <router-link to="/coaches/c1/contact">Contact</router-link>
+   </div>
+</template>
--- ekorki/frontend/src/pages/coaches/CoachesList.vue
+++ ekorki/frontend/src/pages/coaches/CoachesList.vue
@@ -1,53 +1,53 @@
 <template>
-   <section>
-    FILTR
-   </section> 
-   <section>
-      <div class="controls">
-         <button>Odśwież</button>
-      
-      </div>
-      
-   <ul v-if="hasCoaches">
-    <coach-item v-for="coach in filteredCoaches" 
-    :key="coach.id" 
-    :id="coach.id" 
-    :first-name="coach.firstName" 
-    :last-name="coach.lastName"
-    :rate="coach.hourlyRate"
-    :areas="coach.areas">
-   </coach-item>
-   </ul>
-   <h3 v-else>Brak korepetytorów</h3>
-   </section>
+   <div>
+      <section>FILTR</section>
+      <section>
+         <div class="controls">
+            <button>Odśwież</button>
+         </div>
+         <ul v-if="hasCoaches">
+            <coach-item
+               v-for="coach in filteredCoaches"
+               :key="coach.id"
+               :id="coach.id"
+               :first-name="coach.firstName"
+               :last-name="coach.lastName"
+               :rate="coach.hourlyRate"
+               :areas="coach.areas"
+            >
+            </coach-item>
+         </ul>
+         <h3 v-else>Brak korepetytorów</h3>
+      </section>
+   </div>
 </template>
 
 <script>
-import CoachItem from '../../components/coaches/CoachItem.vue'
+import CoachItem from "../../components/coaches/CoachItem.vue";
 export default {
    components: {
-      CoachItem
+      CoachItem,
    },
    computed: {
       filteredCoaches() {
-         return this.$store.getters['coaches/coaches'];
+         return this.$store.getters["coaches/coaches"];
       },
       hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
-   }
-   }
-   
-}
+         return this.$store.getters["coaches/hasCoaches"];
+      },
+   },
+};
 </script>
 
 <style>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
+   list-style: none;
+   margin: 0;
+   padding: 0;
 }
 
 .controls {
-  display: flex;
-  justify-content: space-between;
-}</style>
+   display: flex;
+   justify-content: space-between;
+}
+</style>
