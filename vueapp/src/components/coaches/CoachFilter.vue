38a2bba40ff2eef816a80fff222da992d682a407
Bartosz Troń <123635290+Bartiiio@users.noreply.github.com>
2023-06-01 20:23:25 +0200

    zmiany wyglądowe; dodane panel uzytkownika i wyloguj do header

--- vueapp/src/components/coaches/CoachFilter.vue
+++ vueapp/src/components/coaches/CoachFilter.vue
@@ -1,60 +1,127 @@
 <template>
-    <base-card>
-        <h2>Znajdź swojego korepetytora</h2>
-        <span class="filter-options">
-            <input type="checkbox" id="matematyka" v-model="filters.matematyka" @change="setFilter" />
-            <label for="matematyka">Matematyka</label>
-        </span>
-        <span class="filter-options">
-            <input type="checkbox" id="geografia" v-model="filters.geografia" @change="setFilter" />
+   <base-card>
+      <h2>Znajdź swojego korepetytora</h2>
+      <div class="filters">
+         <div class="checkbox-wrapper-47">
+            <input
+               type="checkbox"
+               id="cb-47"
+               v-model="filters.matematyka"
+               @change="setFilter"
+            />
+            <label for="cb-47">Matematyka</label>
+         </div>
+         <div class="checkbox-wrapper-47">
+            <input
+               type="checkbox"
+               id="geografia"
+               v-model="filters.geografia"
+               @change="setFilter"
+            />
             <label for="geografia">Geografia</label>
-        </span>
-        <span class="filter-options">
-            <input type="checkbox" id="fizyka" v-model="filters.fizyka" @change="setFilter" />
+         </div>
+         <div class="checkbox-wrapper-47">
+            <input
+               type="checkbox"
+               id="fizyka"
+               v-model="filters.fizyka"
+               @change="setFilter"
+            />
             <label for="fizyka">Fizyka</label>
-        </span>
-    </base-card>
+         </div>
+      </div>
+   </base-card>
 </template>
-  
+
 <script>
 export default {
-    emits: ['change-filter'],
-    data() {
-        return {
-            filters: {
-                matematyka: false,
-                geografia: false,
-                fizyka: false,
-            },
-        };
-    },
-    methods: {
-        setFilter() {
-            this.$emit('change-filter', this.filters);
-        },
-    },
+   emits: ["change-filter"],
+   data() {
+      return {
+         filters: {
+            matematyka: false,
+            geografia: false,
+            fizyka: false,
+         },
+      };
+   },
+   methods: {
+      setFilter() {
+         this.$emit("change-filter", this.filters);
+      },
+   },
 };
 </script>
-  
+
 <style scoped>
+.filters {
+   display: flex;
+}
 h2 {
-    margin: 0.5rem 0;
+   margin: 0.5rem 0;
 }
 
 .filter-options {
-    margin-right: 1rem;
+   margin-right: 1rem;
 }
 
 .filter-options label,
 .filter-options input {
-    vertical-align: middle;
+   vertical-align: middle;
 }
 
 .filter-options label {
-    margin-left: 0.25rem;
+   margin-left: 0.25rem;
 }
 
 .filter-options.active label {
-    font-weight: bold;
+   font-weight: bold;
+}
+.checkbox-wrapper-47 {
+   margin: 5px;
+}
+.checkbox-wrapper-47 input[type="checkbox"] {
+   display: none;
+   visibility: hidden;
+}
+
+.checkbox-wrapper-47 label {
+   position: relative;
+   padding-left: 2em;
+   padding-right: 1em;
+   line-height: 2;
+   cursor: pointer;
+   display: inline-flex;
+}
+
+.checkbox-wrapper-47 label:before {
+   box-sizing: border-box;
+   content: " ";
+   position: absolute;
+   top: 0.3em;
+   left: 0;
+   display: block;
+   width: 1.4em;
+   height: 1.4em;
+   border: 2px solid #9098a9;
+   border-radius: 6px;
+   z-index: -1;
+}
+
+.checkbox-wrapper-47 input[type="checkbox"]:checked + label {
+   padding-left: 1em;
+   color: #0f5229;
+}
+.checkbox-wrapper-47 input[type="checkbox"]:checked + label:before {
+   top: 0;
+   width: 100%;
+   height: 2em;
+   background: #b7e6c9;
+   border-color: #2cbc63;
+}
+
+.checkbox-wrapper-47 label,
+.checkbox-wrapper-47 label::before {
+   transition: 0.25s all ease;
 }
 </style>
--- vueapp/src/components/layout/TheHeader.vue
+++ vueapp/src/components/layout/TheHeader.vue
@@ -7,28 +7,31 @@
          </h1>
       </div>
       <div class="logins">
-         <router-link to="/register" v-if="!isLoggedIn">
-            <button class="register">
-               Rejestracja
+         <div class="beforelogged">
+            <router-link to="/register" v-if="!isLoggedIn">
+               <button class="register">Rejestracja</button>
+            </router-link>
+            <router-link to="/login" v-if="!isLoggedIn">
+               <button class="login">Logowanie</button>
+            </router-link>
+         </div>
+         <div class="loggedUser">
+            <img src="../../assets/user.png" alt="" class="logged__User" />
+            <p class="myAccount">Moje Konto</p>
+            <button class="login" v-if="isLoggedIn" @click="logout()">
+               Wyloguj
             </button>
-         </router-link>
-         <router-link to="/login" v-if="!isLoggedIn">
-            <button class="login">
-               Logowanie
-            </button>
-         </router-link>
-         <button class="login"  v-if="isLoggedIn" @click="logout()">
-            Wyloguj
-         </button>
+            <button class="login">Wyloguj</button>
+         </div>
       </div>
    </div>
 </template>
 
 <script setup>
-import { useAuthStore } from '@/store';
-import { storeToRefs } from 'pinia';
+import { useAuthStore } from "@/store";
+import { storeToRefs } from "pinia";
 const store = useAuthStore();
-const { isLoggedIn } = storeToRefs(store)
+const { isLoggedIn } = storeToRefs(store);
 const { logout } = store;
 </script>
 
@@ -64,13 +67,45 @@ const { logout } = store;
    padding: 10px;
    height: 60px;
    width: 100px;
-   padding: 10px;
    border-radius: 50%;
    background-color: transparent;
 }
 .logins {
    text-align: right;
    margin-right: 15%;
+}
+.loggedUser {
+   margin-left: 25%;
+   display: grid;
+   grid-template-columns: 40% 30% 1fr;
+   justify-content: start;
+   align-items: start;
+}
+
+.loggedUser img {
+   justify-self: end;
+   height: 30px;
+   width: 30px;
+   margin: 15px;
+   margin-left: 0;
+}
+
+.loggedUser p {
+   text-align: left;
+   margin: 15px;
+   padding: 5px;
+   grid-column-start: 2;
+   grid-column-end: 3;
+   margin-left: 0px;
+}
+
+.loggedUser button {
+   grid-column-start: 3;
+   grid-column-end: 4;
+}
+
+.beforelogged {
+   display: none;
 }
 .register {
    cursor: pointer;
